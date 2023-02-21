7623f7f451390003d0e24e636a6095e477444f4d
TiagoBarbosa88 <sombr43@gmail.com>
2023-02-21 16:38:18 -0300

    create method

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
 import { RedDirective } from './directives/red.directive';
 import { ForDirective } from './directives/for.directive';
 import { ProductCreateComponent } from './components/products/product-create/product-create.component';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -42,7 +43,8 @@ import { ProductCreateComponent } from './components/products/product-create/pro
     MatListModule,
     MatCardModule,
     MatButtonModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/components/products/product-create/product-create.component.ts
+++ frontend/src/app/components/products/product-create/product-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductService } from '../../product/product.service';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-product-create',
@@ -9,20 +10,28 @@ import { ProductService } from '../../product/product.service';
 })
 export class ProductCreateComponent implements OnInit {
 
-   constructor(
+  product: Product = {
+    name: '',
+    price: 0
+  }
+
+  constructor(
     private productService: ProductService,
     private router: Router
-    ) { }
+  ) { }
 
   ngOnInit(): void {
 
-   }
+  }
 
-   createProduct(): void {
-    this.productService.showMessage('Produto criado');
-   }
+  createProduct(): void {
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto criado')
+      this.router.navigate(['/products'])
+    })
+  }
 
-   cancel(): void {
+  cancel(): void {
     this.router.navigate(['/products'])
-   }
+  }
 }
