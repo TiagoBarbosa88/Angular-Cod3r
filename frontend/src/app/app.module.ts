9095a947d1f6401863a72cc29f602bcbc7a2053e
TiagoBarbosa88 <sombr43@gmail.com>
2023-02-23 06:45:26 -0300

    created Schematics

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -26,6 +26,10 @@ import { FormsModule } from '@angular/forms'
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input';
 import { ProductReadComponent } from './components/products/product-read/product-read.component';
+import { ProductRead2Component } from './components/products/product-read2/product-read2.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 
 @NgModule({
@@ -40,7 +44,8 @@ import { ProductReadComponent } from './components/products/product-read/product
     ForDirective,
     ProductCreateComponent,
     ProductCrudComponent,
-    ProductReadComponent
+    ProductReadComponent,
+    ProductRead2Component
   ],
   imports: [
     BrowserModule,
@@ -55,7 +60,10 @@ import { ProductReadComponent } from './components/products/product-read/product
     HttpClientModule,
     FormsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
