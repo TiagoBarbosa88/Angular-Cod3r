5c96c1bda38b9ca8207a1425b6e3c2e6cabdd96f
TiagoBarbosa88 <sombr43@gmail.com>
2023-02-24 06:09:37 -0300

    created table

--- frontend/src/app/components/products/product-read/product-read.component.ts
+++ frontend/src/app/components/products/product-read/product-read.component.ts
@@ -10,6 +10,7 @@ import { ProductService } from '../../product/product.service';
 export class ProductReadComponent implements OnInit {
 
   products: Product[] = [];
+  displayedColumns = ['id', 'name', 'price']
 
   constructor(private productService: ProductService) { }
 
